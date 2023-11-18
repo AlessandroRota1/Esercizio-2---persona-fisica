0280aa1e8c59d3aeafdc57adab5abae1109e72ab
Alessandro Rota <rotaale05102006@gmail.com>
2023-11-18 17:23:26 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio 2 - persona fisica/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_2___persona_fisica
+{
+    class dati
+    {
+        public string codice;
+        public string nome;
+        public string cognome;
+        public bool registrato;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

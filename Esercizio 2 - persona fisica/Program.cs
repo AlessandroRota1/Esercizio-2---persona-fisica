73d23c0161f21cafdf843bed5529ab11db9466bd
Alessandro Rota <rotaale05102006@gmail.com>
2023-11-18 17:29:09 +0100

    Implementazione persona fisica

--- Esercizio 2 - persona fisica/Program.cs
+++ Esercizio 2 - persona fisica/Program.cs
@@ -12,11 +12,40 @@ namespace Esercizio_2___persona_fisica
         public string nome;
         public string cognome;
         public bool registrato;
+        public dati(string codice, string cognome, string nome)
+        {
+            this.codice = codice;
+            this.cognome = cognome;
+            this.nome = nome;
+            registrato = false;
+        }
+        public void Registra()
+        {
+            registrato = true;
+            Console.WriteLine("Persona registrata con successo.");
+        }
+        public void Mostra()
+        {
+            Console.WriteLine("Codice: {0}", codice);
+            Console.WriteLine("Cognome: {0}", cognome);
+            Console.WriteLine("Nome: {0}", nome);
+            Console.WriteLine("Registrato: {0}", registrato ? "Sì" : "No");
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            dati persona = new dati("21323", "Rota", "Alessandro");
+
+            Console.WriteLine("Dati Anagrafici prima della registrazione:");
+            persona.Mostra();
+            Console.WriteLine("Clicca un tasto per confermare la registrazione");
+            Console.ReadLine();
+            persona.Registra();
+
+            Console.WriteLine("\nDati Anagrafici dopo la registrazione:");
+            persona.Mostra();
         }
     }
 }
